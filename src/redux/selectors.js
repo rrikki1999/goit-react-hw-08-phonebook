117a8845e8b97bbd9d9ebc79ad77a5359ef55efa
Nadya <timrova.nadiia1622@vu.cdu.edu.ua>
2024-02-13 22:29:35 +0200

    auth

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -12,3 +12,10 @@ export const selectVisibleContacts = createSelector(
     );
   }
 );
+
+export const selectAuthToken = state => state.auth.token;
+export const selectAuthUserData = state => state.auth.userData;
+export const selectAuthIsLoggedIn = state => state.auth.isLoggedIn;
+export const selectAuthError = state => state.auth.error;
+export const selectAuthisLoading = state => state.auth.isLoading;
+
